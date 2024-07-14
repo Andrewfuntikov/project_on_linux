4ddd516467773df6ea5ad3c6b0a6fddaa2bc0672
АндрейLInux <andhunter77@gmail.com>
2024-07-14 16:08:38 +0500

    Добавил файлы в гит

---
+++ django_project_movie_siye/movie_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+from .models import Movie
+from django.views.generic import ListView, DetailView
+from .forms import ReviewForm
+
+
+class MovieView(ListView):
+    """Список фильмов"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = "movies/movie_list.html"
+
+
+class MovieDetailView(DetailView):
+    """Полное описание фильма"""
+
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, 'movies/movie_detail.html', {"movie": movie})
+
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            if request.POST.get("parent", None):
+                form.parent_id = int(request.POST.get("parent"))
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
